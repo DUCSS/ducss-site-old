a6aef03b8fbdf2052fe85551c668ec492846494d
Kevin Baker <eire332kevin@gmail.com>
2013-10-10 00:57:19 +0100

    Cleaned up unused files. Added the requirements txt file. Changed the default dev database to SQLite.

--- project/theme/views.py
+++ project/theme/views.py
@@ -7,7 +7,7 @@ def home(request, template=None):
 	'''Home page'''
 	context = RequestContext(request)
 
-	context['events'] = Event.objects.all()
+	#context['events'] = Event.objects.all()
 
 	return render_to_response(template, context_instance=context)
 
